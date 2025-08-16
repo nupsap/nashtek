083ea598bb55937183ccbc108cd88a5f6f641ff5
nupsap <nupsap@gmail.com>
2025-08-15 23:59:30 -0400

    Delete nashtek_site directory

--- nashtek_site/assets/script.js
+++
@@ -1,6 +0,0 @@
-function toggleNav(){
-  const el = document.getElementById('navLinks');
-  if(!el) return;
-  const isShown = getComputedStyle(el).display !== 'none';
-  el.style.display = isShown ? 'none' : 'flex';
-}
